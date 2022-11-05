Fixed antithetic variables issue (YAY variance is reduced)

inverse_transform_sampling.py
@@ -17,7 +17,9 @@ def generate_exponential(lmbda: float):
     """
 
     randomNumber = np.random.uniform(low = 0.0, high = 1.0)
-    return stats.expon.ppf(q = randomNumber, scale = 1 / lmbda)
+    return - (1 / lmbda) * np.log(randomNumber)
+    # return stats.expon.ppf(q = randomNumber, scale = 1 / lmbda)
+
 
 def generate_exponential_antithetic(lmbda: float):
     """This function generates an exponential random variable with the provided parameters.
@@ -27,7 +29,8 @@ def generate_exponential_antithetic(lmbda: float):
     """
 
     randomNumber = np.random.uniform(low = 0.0, high = 1.0)
-    return [stats.expon.ppf(q = randomNumber, scale = 1 / lmbda), stats.expon.ppf(q = 1 - randomNumber, scale = 1 / lmbda)]
+    return ((- (1 / lmbda) * np.log(1 - randomNumber)) + (- (1 / lmbda) * np.log(randomNumber))) / 2
+    # return (stats.expon.ppf(q = randomNumber, scale = 1 / lmbda) + stats.expon.ppf(q = 1 - randomNumber, scale = 1 / lmbda)) / 2
 
 
 def generate_normal(loc: float = 0, scale: float = 1):
@@ -62,4 +65,5 @@ def generate_binomial_antithetic(n: int, p: float = 0.5):
     """
 
     randomNumber = np.random.uniform(low = 0.0, high = 1.0)
-    return [stats.binom.ppf(q = randomNumber, n = n, p = p), stats.binom.ppf(q = 1 - randomNumber, n = n, p = p)]
+    return stats.binom.ppf(q = randomNumber, n = n, p = p)
+    # return (stats.binom.ppf(q = randomNumber, n = n, p = p) + stats.binom.ppf(q = 1 - randomNumber, n = n, p = p)) / 2

simulation.py
@@ -62,35 +62,23 @@ def run_experiment(
 
         # arrival_lambda = average number of customers in a time period
         if variance_reduction == 'Antithetic Variables':
-            interarrival_times_list = [generate_exponential_antithetic(arrival_lambda) for _ in range(serving_limit)]
-            serving_times_list = [[x+1 for x in generate_binomial_antithetic(n=bus_stops)] for _ in range(serving_limit)]
-
-            for interarrival_times, serving_times in zip(zip(*interarrival_times_list), zip(*serving_times_list)):
-                customer_history = run_simulation(bus_seats, bus_stops, interarrival_times, serving_times, serving_limit, verbose)
-                customer_history = aggregate_results(customer_history)
-                experiment_results.append({
-                    'arrival_lambda': arrival_lambda,
-                    'bus_seats': bus_seats,
-                    'bus_stops': bus_stops,
-                    'average_waiting_time': np.mean(customer_history['waiting_time']),
-                    'average_time_in_system': np.mean(customer_history['time_in_system']),
-                    'average_customers_upon_arrival': np.mean(customer_history['customers_upon_arrival'])
-                })
+            interarrival_times = [generate_exponential_antithetic(arrival_lambda) for _ in range(serving_limit)]
+            serving_times = [generate_binomial_antithetic(n=bus_stops)+1 for _ in range(serving_limit)]
 
         else: # Standard MC
             interarrival_times = [generate_exponential(arrival_lambda) for _ in range(serving_limit)]
             serving_times = [generate_binomial(n=bus_stops)+1 for _ in range(serving_limit)]
 
-            customer_history = run_simulation(bus_seats, bus_stops, interarrival_times, serving_times, serving_limit, verbose)
-            customer_history = aggregate_results(customer_history)
-            experiment_results.append({
-                'arrival_lambda': arrival_lambda,
-                'bus_seats': bus_seats,
-                'bus_stops': bus_stops,
-                'average_waiting_time': np.mean(customer_history['waiting_time']),
-                'average_time_in_system': np.mean(customer_history['time_in_system']),
-                'average_customers_upon_arrival': np.mean(customer_history['customers_upon_arrival'])
-            })
+        customer_history = run_simulation(bus_seats, bus_stops, interarrival_times, serving_times, serving_limit, verbose)
+        customer_history = aggregate_results(customer_history)
+        experiment_results.append({
+            'arrival_lambda': arrival_lambda,
+            'bus_seats': bus_seats,
+            'bus_stops': bus_stops,
+            'average_waiting_time': np.mean(customer_history['waiting_time']),
+            'average_time_in_system': np.mean(customer_history['time_in_system']),
+            'average_customers_upon_arrival': np.mean(customer_history['customers_upon_arrival'])
+        })
 
     results = pd.DataFrame(experiment_results)
 

test.py
@@ -1,3 +1,4 @@
+from inverse_transform_sampling import *
 import numpy as np
 import scipy.stats as stats
 
@@ -9,15 +10,34 @@ import scipy.stats as stats
 # stratified sampling
 # importance sampling
 
-def generate_exponential(lmbda: float):
-    """This function generates an exponential random variable with the provided parameters.
+print("Hello!")
 
-    Parameters----
-    lmbda : the 1/scale parameter for your exponential distribution
-    """
+samples = 1000
+iterations = 100
+a_list = []
+b_list = []
 
+def exp(lmbda):
     randomNumber = np.random.uniform(low = 0.0, high = 1.0)
-    return stats.expon.ppf(q = randomNumber, scale = 1 / lmbda)
+    return - (1 / lmbda) * np.log(randomNumber)
 
-for interarrival_times, serving_times in zip(zip(*[[0,1],[2,3],[4,5]]), zip(*[[7,8],[9,10],[11,12]])):
-    print(list(interarrival_times), list(serving_times))
+def exp_antithetic(lmbda):
+    randomNumber = np.random.uniform(low = 0.0, high = 1.0)
+    return ((- (1 / lmbda) * np.log(1 - randomNumber)) + (- (1 / lmbda) * np.log(randomNumber))) / 2
+
+for i in range(iterations):
+    a = [exp(5) for _ in range(samples)]
+    # a = [generate_exponential(5) for _ in range(samples)]
+    # a_list.append(np.mean(a))
+    a_list.extend(a)
+    if i % 2 == 0:
+        b = [exp_antithetic(5) for _ in range(samples)]
+        # b = [generate_exponential_antithetic(5) for _ in range(samples)]
+        # b_list.append(np.mean(b[0]))
+        # b_list.append(np.mean(b[1]))
+        # b0, b1 = zip(*b)
+        b_list.extend(b)
+        # b_list.extend(b1)
+
+print(f"Stats for a are len={len(a_list)}, mean={np.mean(a_list)} and std={np.std(a_list)} with variance={np.std(a_list) ** 2}")
+print(f"Stats for b are len={len(b_list)}, mean={np.mean(b_list)} and std={np.std(b_list)} with variance={np.std(b_list) ** 2}")