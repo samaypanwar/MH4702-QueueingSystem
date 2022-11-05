f534d41ccea41f935b531a677a463dce0a303fde
kimtxn <kimberley028@gmail.com>
2022-11-05 20:07:46 +0800

    Testing antithetic variables

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import pandas as pd
 from simulation import run_experiment
+from tqdm import tqdm
 
 if __name__ == '__main__':
 
     verbose = False
     time_limit = float("inf") # no time limit
     serving_limit = 1000
-    iterations = 1000
+    iterations = 500
     standard_mc_results = []
     antithetic_variables_results = []
 
-    for arrival_lambda in [5, 10, 20]:
-        for bus_seats in [10, 20, 50]:
-            for bus_stops in [5, 10, 20]:
+    for arrival_lambda in [5, 20, 50]:
+        for bus_seats in [10, 50, 100]:
+            for bus_stops in tqdm([5, 10, 20]):
 
                 antithetic_variables_results.append(run_experiment(
                     iterations=iterations // 2,
@@ -23,6 +24,8 @@ if __name__ == '__main__':
                     serving_limit=serving_limit,
                     verbose=verbose))
 
+                print(antithetic_variables_results[-1])
+
                 standard_mc_results.append(run_experiment(
                     iterations=iterations,
                     arrival_lambda=arrival_lambda,
@@ -32,6 +35,8 @@ if __name__ == '__main__':
                     serving_limit=serving_limit,
                     verbose=verbose))
 
+                print(standard_mc_results[-1])
+
     antithetic_variables_df = pd.DataFrame(antithetic_variables_results)
     standard_mc_df = pd.DataFrame(standard_mc_results)
 
@@ -39,6 +44,9 @@ if __name__ == '__main__':
     print(standard_mc_df)
     print("\nAntithetic Variables results:")
     print(antithetic_variables_df)
+
+    antithetic_variables_df.to_csv('antithetic_variables_results.csv')
+    standard_mc_df.to_csv('standard_mc_results.csv')
                 
 
 # TODO: Add logging statements
